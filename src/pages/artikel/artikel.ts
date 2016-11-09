546464c014b577cd2fd31caccd95c39bf9a3453d
Muhammad Aslam A <aslamabdurrohim@gmail.com>
2016-11-09 20:59:11 +0700

    menampilkan artikel

--- src/pages/artikel/artikel.ts
+++ src/pages/artikel/artikel.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+
+import { ActionSheetController } from 'ionic-angular';
 
 /*
   Generated class for the Artikel page.
@@ -12,11 +15,44 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'artikel.html'
 })
 export class ArtikelPage {
+	public artikel;
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController, public http: Http, public actionSheetCtrl: ActionSheetController) {
+
+  }
 
   ionViewDidLoad() {
     console.log('Hello ArtikelPage Page');
+    this.http.get('http://cybex.agri.web.id/api/all_artikel.php').subscribe(res => {
+      this.artikel = res.json();
+    });
+  }
+
+  presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Pilihan',
+      buttons: [
+        {
+          text: 'Tulis Artikel',
+          role: 'tulisArtikel',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        },{
+          text: 'Tanya/Diskusi',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        },{
+          text: 'Batal',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 
 }
