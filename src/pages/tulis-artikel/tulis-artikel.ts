04409c5856784fb830b81d3c28920d97a986bb1d
MUHAMMAD ASLAM A <aslamabdurrohim@gmail.com>
2017-01-22 02:22:19 +0700

    tambah input dan upload gambar

--- src/pages/tulis-artikel/tulis-artikel.ts
+++ src/pages/tulis-artikel/tulis-artikel.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { NavController, App, ActionSheetController, AlertController, ToastController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import { Camera } from 'ionic-native';
-import { ImagePicker } from 'ionic-native';
 
 import { UserData } from '../../providers/user-data';
 import { LoginPage } from '../login/login';
@@ -17,11 +16,14 @@ export class TulisArtikelPage {
   public judul_artikel: string;
   public input: string;
   public id_kategori = 1;
-  public id_topik = 1;
-  public id_komoditas: any;
-  public id_user_input;
+  public id_topik = 0;
+  public id_komoditas = 0;
+  public id_user_input: any;
   public koms;
+  public tops;
+  public kats;
   public noInput = false;
+  public gambar: string;
 
   constructor(public toastCtrl: ToastController, public navCtrl: NavController, public app: App, public http: Http, public alertCtrl: AlertController, public actionSheetCtrl: ActionSheetController, public userData: UserData) {
 
@@ -31,6 +33,14 @@ export class TulisArtikelPage {
     console.log('Hello TulisArtikelPage Page');
     this.http.get('http://cybex.agri.web.id/api/all_komoditas.php').subscribe(res => {
       this.koms = res.json();
+    });
+
+    this.http.get('http://cybex.agri.web.id/api/all_topik.php').subscribe(res => {
+      this.tops = res.json();
+    });
+
+    this.http.get('http://cybex.agri.web.id/api/all_kategori.php').subscribe(res => {
+      this.kats = res.json();
     });
 
     if(this.userData.loginState){
@@ -59,40 +69,53 @@ export class TulisArtikelPage {
   }
 
   dariKamera() {
-  	console.log('getPicture Clicked');
-    let options = '';
-    Camera.getPicture(options).then((imageData) => {
+    Camera.getPicture({
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      targetWidth: 1000,
+      targetHeight: 1000
+    }).then((imageData) => {
      // imageData is either a base64 encoded string or a file URI
      // If it's base64:
      let base64Image = 'data:image/jpeg;base64,' + imageData;
-     console.log(base64Image);
+     this.gambar = base64Image;
     }, (err) => {
-     // Handle error
+     console.log(err);
+     alert('Tidak bisa ambil gambar');
     });
   }
 
   dariGaleri() {
-    let options = '';
-    ImagePicker.getPictures(options).then((results) => {
-      for (var i = 0; i < results.length; i++) {
-          console.log('Image URI: ' + results[i]);
-      }
-    }, (err) => { });
+    Camera.getPicture({
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+      targetWidth: 1000,
+      targetHeight: 1000
+    }).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64:
+     let base64Image = 'data:image/jpeg;base64,' + imageData;
+     this.gambar = base64Image;
+    }, (err) => {
+     console.log(err);
+     alert('Tidak bisa ambil gambar');
+    });
   }
 
   kirim() {
-      if(this.isi_artikel == undefined || this.judul_artikel == undefined || this.id_komoditas == undefined  || !this.isi_artikel || !this.judul_artikel || !this.id_komoditas){
+      if(this.isi_artikel == undefined || this.judul_artikel == undefined || this.id_kategori == undefined || !this.isi_artikel || !this.judul_artikel || !this.id_kategori){
         this.noInput = true;
-      }else{
-      this.id_user_input = this.userData.ids;
-      this.input = JSON.stringify({isi_artikel: this.isi_artikel, judul_artikel: this.judul_artikel,
-        id_kategori: this.id_kategori, id_topik: this.id_topik, id_komoditas: this.id_komoditas, id_user_input: this.id_user_input});
-      console.log(this.input);
-      this.http.post("http://cybex.agri.web.id/api/tulis_artikel.php", this.input).subscribe(data => {
-          let v = data.json();
-          this.showToast(v['message']);
-          this.navCtrl.pop();
-      });
+      }
+      else{
+        this.id_user_input = this.userData.ids;
+        this.input = JSON.stringify({isi_artikel: this.isi_artikel, judul_artikel: this.judul_artikel,
+          id_kategori: this.id_kategori, id_topik: this.id_topik, id_komoditas: this.id_komoditas, id_user_input: this.id_user_input, gambar: this.gambar});
+        console.log(this.input);
+        this.http.post("http://cybex.agri.web.id/api/tulis_artikel.php", this.input).subscribe(data => {
+            let v = data.json();
+            this.showToast(v['message']);
+            this.navCtrl.pop();
+        });
       }
   }
 
@@ -120,19 +143,19 @@ export class TulisArtikelPage {
     let actionSheet = this.actionSheetCtrl.create({
       title: 'Pilihan',
       buttons: [
-        // {
-        //   text: 'Kamera',
-        //   role: 'tulisArtikel',
-        //   handler: () => {
-        //     console.log('Tulis Artikel clicked');
-        //     this.dariKamera();
-        //   }
-        // },
+        {
+          text: 'Kamera',
+          role: 'tulisArtikel',
+          handler: () => {
+            console.log('Kamera clicked');
+            this.dariKamera();
+          }
+        },
         {
           text: 'Galeri',
           role: 'tulisDiskusi',
           handler: () => {
-            console.log('Tulis Diskusi clicked');
+            console.log('Galeri clicked');
             this.dariGaleri();
           }
         },{
